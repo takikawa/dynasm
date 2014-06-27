8e386ccfe49914352c363184f0edf8f17d1ab965
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-06-27 00:52:05 +0000

    x64 build script: added -fPIC

--- csrc/dynasm/build-linux64.sh
+++ csrc/dynasm/build-linux64.sh
@@ -1,1 +1,1 @@
-gcc -O2 -s -static-libgcc dasm_x86.c -shared -o ../../bin/linux64/libdasm_x86.so -DDASM_CHECKS
+gcc -O2 -s -static-libgcc dasm_x86.c -fPIC -shared -o ../../bin/linux64/libdasm_x86.so -DDASM_CHECKS
